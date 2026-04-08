b9cfa8df1d7091e24929fc1a64e068d586d4ca1a
aumilkyeong <aumilkyeong@gmail.com>
2026-04-08 23:18:45 +0900

    feat: add LocaleLayout and locale-based routing

--- apps/ui-kit/src/App.tsx
+++ apps/ui-kit/src/App.tsx
@@ -1,24 +1,19 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router';
+import { LocaleLayout } from '@/layouts/LocaleLayout';
 import { DocsLayout } from '@/layouts/DocsLayout';
 import { componentRoutes } from '@/routes';
-
-function HomePage() {
-  return <Navigate to={`/components/${componentRoutes[0].path}`} replace />;
-}
 
 export function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/components" element={<DocsLayout />}>
-          {componentRoutes.map((route) => (
-            <Route
-              key={route.path}
-              path={route.path}
-              element={<route.component />}
-            />
-          ))}
+        <Route path="/" element={<Navigate to="/ko/components/autocomplete" replace />} />
+        <Route path="/:locale/components" element={<LocaleLayout />}>
+          <Route element={<DocsLayout />}>
+            {componentRoutes.map((route) => (
+              <Route key={route.path} path={route.path} element={<route.component />} />
+            ))}
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
